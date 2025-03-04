b9d69cbc8e148776fe9a8ac75139b4de70fe21eb
Martin Løyche <martin.doesnt.exist@gmail.com>
2025-03-04 21:14:28 +0100

    Reverting a lot of things

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
-import createMDX from "@next/mdx";
-import { remarkPlugins } from "./src/mdx/remark.mjs";
-
 const nextConfig: NextConfig = {
   /* config options here */
-  pageExtensions: ["md", "mdx", "ts", "tsx"],
+  pageExtensions: ["ts", "tsx"],
 };
 
-const withMdx = createMDX({
-  options: {
-    remarkPlugins,
-  },
-});
-
-export default withMdx(nextConfig);
+export default nextConfig;
--- src/mdx/remark.mjs
+++
@@ -1,9 +0,0 @@
-import { mdxAnnotations } from "mdx-annotations";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
-
-export const remarkPlugins = [
-  mdxAnnotations.remark,
-  remarkFrontmatter,
-  remarkMdxFrontmatter,
-];
